4bf2a922347ecb787a605eb029934c5659d814da
vadimkorzik <k.vadim.exe@gmail.com>
2012-04-30 23:07:07 +0300

    blah - blah

--- src/Brootforce/network/NetworkManager.java
+++ src/Brootforce/network/NetworkManager.java
@@ -1,8 +1,33 @@
 package Brootforce.network;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * User: Vadim | Date: 14.04.12 | Time: 22:43
  */
 public class NetworkManager {
+
+    public void connect() {
+        byte buffer[] = new byte[2048];
+        try {
+            Socket socket = new Socket("127.0.0.1", 19191);
+
+            InputStream in = socket.getInputStream();
+            OutputStream out = socket.getOutputStream();
+
+            while (true) {
+                buffer[0] = 35;
+                buffer[1] = 81;
+                buffer[2] = 127;
+                out.write(buffer, 0, 3);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
