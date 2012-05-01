34f282287656d0a630e74a9070cb79e652f2eb29
vadimkorzik <k.vadim.exe@gmail.com>
2012-05-01 17:16:36 +0300

    blah - blah

--- src/Brootforce/core/BrootforceManager.java
+++ src/Brootforce/core/BrootforceManager.java
@@ -4,4 +4,7 @@ package Brootforce.core;
  * User: Vadim | Date: 14.04.12 | Time: 21:52
  */
 public class BrootforceManager {
+
+    private int countIntervals;
+    private String alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 }
